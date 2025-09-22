1011cc2286ad60c0a2ca7f9ba5733a3f01109875
Aman Singh <amansingh99170@gmail.com>
2025-09-23 00:03:04 +0530

    initial commit

---
+++ src/main/java/com/apigateway/getcitations/GetcitationsApplication.java
@@ -0,0 +1,13 @@
+package com.apigateway.getcitations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetcitationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetcitationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apigateway/getcitations/GetcitationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigateway.getcitations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetcitationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
